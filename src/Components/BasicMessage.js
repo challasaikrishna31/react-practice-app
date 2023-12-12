d94cbacf151c25a2db6dec00720887b65304c928
skchalla <csaikrishnareddy@mirabeltechnologies.com>
2023-12-12 10:40:37 +0530

    formatted basic message

--- src/Components/BasicMessage.js
+++ src/Components/BasicMessage.js
@@ -1,34 +1,12 @@
 import React, { useEffect, useState } from 'react'
 
 const BasicMessage = () => {
-    const [counter, setCounter] = useState(0);
-    const [action, setAction] = useState("");
-    useEffect(() => {
-        let interval;
-        if (action == "start" || action == "resume") {
-            interval = setInterval(() => {
-                setCounter((prev) => prev + 1)
-            }, 1000)
-        } else if (action == "reset") {
-            setCounter(0);
-            clearInterval(interval);
-        } else if (action == "pause") {
-            clearInterval(interval);
-        }
-
-        return () => {
-            clearInterval(interval);
-        }
-    }, [action])
-
     return (
         <div className="p-2 m-2 flex flex-col justify-around bg-gray-100 w-96 h-96 items-center">
             <div className="font-bold">
                     Hello Welcome!!!!!
-                </div>
-
+            </div>
         </div>
-
     )
 }
 
